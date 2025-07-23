21150979e5a704a1fd4850a70d3fa341c2c9245d
Aditya Shashi <adityatulip2@gmail.com>
2025-07-23 23:09:23 +0530

    Created user register API

--- models/userModel.js
+++ models/userModel.js
@@ -3,29 +3,30 @@ import validator from 'validator';
 
 //schema
 const userSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:[true,'Name is require']
+    name: {
+        type: String,
+        required: [true, 'Name is require']
     },
-    lastName:{
-        type:String,
+    lastName: {
+        type: String,
     },
-    email:{
-        type:String,
-        requried:[true,'Email is require'],
-        unique:true,
-        validate:validator.isEmail
+    email: {
+        type: String,
+        requried: [true, 'Email is require'],
+        unique: true,
+        validate: validator.isEmail
     },
-    password:{
-        type:string,
-        rewuired:[true,'Password is require'],
+    password: {
+        type: String,
+        rewuired: [true, 'Password is require'],
+        minlength: [6, 'Password is too short']
     },
-    locations:{
-        type:string,
-        default:'India'
+    locations: {
+        type: String,
+        default: 'India'
     },
 },
-{timeStamps:true}
+    { timeStamps: true }
 );
 
-export default mongoose.model('User',userSchema)
+export default mongoose.model('User', userSchema)
--- server.js
+++ server.js
@@ -8,7 +8,9 @@ import morgan from 'morgan'
 
 // files imports
 import connectDB from './config/db.js';
-import testRoutes from './routes/testRoutes.js'
+//routes imports
+import testRoutes from './routes/testRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 //config
 dotenv.config();
 //mongodb connection
@@ -22,7 +24,8 @@ app.use(cors());
 app.use(morgan(`dev`));
 
 //routes
-app.use('/api/v1/test',testRoutes)
+app.use('/api/v1/test', testRoutes);
+app.use('/api/v1/auth', authRoutes);
 //port
 const PORT = process.env.PORT || 8080
 //listen
