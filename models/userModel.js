af1ba6dfa97b7efb08c093699787cff9493651f5
Aditya Shashi <adityatulip2@gmail.com>
2025-07-28 22:29:58 +0530

    Pasword hashing

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import validator from 'validator';
+import bcrypt from 'bcryptjs';
 
 //schema
 const userSchema = new mongoose.Schema({
@@ -23,10 +24,16 @@ const userSchema = new mongoose.Schema({
     },
     locations: {
         type: String,
-        default: 'India'
+        default: 'India',
     },
 },
-    { timeStamps: true }
+    { timestamps: true }
 );
+
+//middlewares
+userSchema.pre('save', async function () {
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt);
+})
 
 export default mongoose.model('User', userSchema)
