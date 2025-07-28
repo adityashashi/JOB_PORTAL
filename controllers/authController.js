129dd346c19e447549d9b5586202c3a3fba91b9d
Aditya Shashi <adityatulip2@gmail.com>
2025-07-28 22:12:35 +0530

    Error handeling

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,28 +1,25 @@
 import userModel from "../models/userModel.js";
 
 export const registerController = async (req, res, next) => {
-    try {
-        const { name, email, password } = req.body
-        //validate
-        if (!name) {
-            next("name is required");
-        } if (!email) {
-            next("email is required");
-        } if (!password) {
-            next("password is required");
-        }
-        const existingUser = await userModel.findOne({ email })
-        if (existingUser) {
-            next("Email already register, Please login")
-        }
-        const user = await userModel.create({ name, email, password })
-        res.status(201).send({
-            success: true,
-            mesaage: 'User Created Successfully',
-            user,
-        });
-    } catch (error) {
-        next(error);
 
-    };
+    const { name, email, password } = req.body
+    //validate
+    if (!name) {
+        next("name is required");
+    } if (!email) {
+        next("email is required");
+    } if (!password) {
+        next("password is required");
+    }
+    const existingUser = await userModel.findOne({ email })
+    if (existingUser) {
+        next("Email already register, Please login")
+    }
+    const user = await userModel.create({ name, email, password })
+    res.status(201).send({
+        success: true,
+        mesaage: 'User Created Successfully',
+        user,
+    });
+
 }
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 //packages imports
-
 import express from 'express'
+import "express-async-errors"
 import dotenv from 'dotenv'
 import colors from 'colors'
 import cors from 'cors'
 import morgan from 'morgan'
+
 
 // files imports
 import connectDB from './config/db.js';
